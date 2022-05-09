978f42c941cb0b0c362cdf9f3d528c554c2a17f4
Gage Clancy <gclancy121@gmail.com>
2022-05-09 15:53:35 -0500

    initial commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todo App</h1>
+        <ul>
+          <li>Walk dog</li>
+          <li>Get better at coding</li>
+          <li>Be happy</li>
+        </ul>
+
+      <form>
+        <input />
+        <button>Add</button>
+        
+      </form>
+      <button>Clear</button>
       </div>
     )
   }
